1e3e28adb3bb4d0fd0d90d25754a5983b6dfcf0f
abhaygupt <abhayrobotics1908@gmail.com>
2022-02-21 16:57:59 +0530

    complete upto 25 min js mastery tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {ChatEngine} from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// *Importing CSS
+import './App.css'
+
+// * Importing new components
+import ChatFeed from './components/ChatFeed';
+
+
+
+const App = () =>{
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="157ecb4e-3a1a-4160-8a40-3afd7f0a2625"
+            userName ="javascript"
+            userSecret="123123"
+
+            renderChatFeed = {(chatAppProps) => <ChatFeed {... chatAppProps}/>
+                            }
+            />
+          )
 }
 
 export default App;
---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,8 @@
+
+const MessageForm = () => {
+  return <div>
+      MesssageForm
+  </div>;
+};
+
+export default MessageForm;
